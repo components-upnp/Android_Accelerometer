52fbea2fcd5cef2318c2a8453760be5a8697fe15
MKostiuk <mathieukostiuk@hotmail.fr>
2017-04-14 15:10:54 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/comkostiuk/accelerometer/App.java
@@ -0,0 +1,13 @@
+package com.example.comkostiuk.accelerometer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class App extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+    }
+}
